43cc7654af1dbf2cb1bcb2795321f53672bca441
Sravanthi Yachareni <sravanthiyachareni@gmail.com>
2024-10-01 13:55:37 +0530

    implemented

--- src/main/java/com/sravs/EmployeeBackendApplication.java
+++ src/main/java/com/sravs/EmployeeBackendApplication.java
@@ -1,13 +1,19 @@
 package com.sravs;
 
+import com.sravs.model.Employee;
+import com.sravs.repository.EmployeeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EmployeeBackendApplication {
+public class EmployeeBackendApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeBackendApplication.class, args);
 	}
+@Autowired
+private EmployeeRepo employeeRepo;
 
 }
