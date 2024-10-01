007c3ebf84f23ecdbf2f020faca34e84ff92a910
Sravanthi Yachareni <sravanthiyachareni@gmail.com>
2024-10-01 13:56:14 +0530

    implemented employee controller

--- src/main/java/com/sravs/controller/EmployeeController.java
+++ src/main/java/com/sravs/controller/EmployeeController.java
@@ -1,20 +1,25 @@
 package com.sravs.controller;
 
+import com.sravs.dto.EmployeeDto;
 import com.sravs.model.Employee;
 import com.sravs.repository.EmployeeRepo;
+import com.sravs.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/employee")
 public class EmployeeController {
     @Autowired
-    EmployeeRepo employeeRepo;
-    @GetMapping
-    public List<Employee> getAll(){
-        return  employeeRepo.findAll();
+    EmployeeService employeeService;
+    @PostMapping
+    public EmployeeDto createEmployee(@RequestBody  EmployeeDto employeeDto){
+        return  employeeService.createEmployee(employeeDto);
+    }
+    @GetMapping("{id}")
+    public  EmployeeDto getEmployeeId(@PathVariable ("id") Long id){
+        return   employeeService.getEmployeeId(id);
     }
 }
